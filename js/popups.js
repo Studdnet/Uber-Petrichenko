391915f225dfac5406dc2d000dc56da0cd2a6ac8
Stud.net <st.codding@gmail.com>
2022-05-29 23:49:26 +0300

    add modal window in header - link contacts

--- js/popups.js
+++ js/popups.js
@@ -1,58 +1,6 @@
-"use strict";
-
-const body = document.querySelector("body");
-const header = document.querySelector(".header");
-const headerBox = document.querySelector(".header__box");
-
-let calcScrollValue = () => {
-	let scrollProgress = document.getElementById("progressButton");
-	let progressValue = document.getElementById("progressButton__value");
-	let pos = document.documentElement.scrollTop;
-	let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-	let scrollValue = Math.round((pos * 100) / calcHeight);
-
-	if (pos > 100) {
-		scrollProgress.style.display = "grid";
-	} else {
-		scrollProgress.style.display = "none";
-	}
-
-	scrollProgress.addEventListener("click", () => {
-		document.documentElement.scrollTop = 0;
-	});
-	scrollProgress.style.background = `conic-gradient(#ffa500 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
-};
-
-window.onload = calcScrollValue;
-window.addEventListener('scroll', calcScrollValue);
-
-// Sticky-анимация
-
-window.addEventListener('scroll', function () {
-	this.scrollY > 20 ? headerBox.classList.add('sticky') : headerBox.classList.remove('sticky');
-});
-
-
-const iconMenu = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.menu__body');
-if (iconMenu) {
-	iconMenu.addEventListener("click", function (e) {
-		document.body.classList.toggle('_lock');
-		iconMenu.classList.toggle('_active');
-		menuBody.classList.toggle('_active');
-	});
-}
-// let menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-// if (menuLinks.length > 0) {
-// 	menuLinks.forEach(menuLink => {
-// 			menuLink.addEventListener("click", onMenuLinkClick);
-// 		}
-// 	};
-
-
-const popupLinks = document.querySelectorAll(".popup-link");
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
 const lockPadding = document.querySelectorAll(".lock-padding");
-const popupCloseIcon = document.querySelectorAll('.close-popup');
 
 let unlock = true;
 
@@ -69,7 +17,7 @@ if (popupLinks.length > 0) {
 		});
 	}
 }
-
+const popupCloseIcon = document.querySelectorAll('.close-popup');
 if (popupCloseIcon.length > 0) {
 	for (let index = 0; index < popupCloseIcon.length; index++) {
 		const el = popupCloseIcon[index];
@@ -116,7 +64,7 @@ function bodyLock() {
 		}
 	}
 	body.style.paddingRight = lockPaddingValue;
-	body.classList.add('_lock');
+	body.classList.add('lock');
 
 	unlock = false;
 	setTimeout(function () {
@@ -133,7 +81,7 @@ function bodyUnLock() {
 			}
 		}
 		body.style.paddingRight = '0px';
-		body.classList.remove('_lock');
+		body.classList.remove('lock');
 	}, timeout);
 
 	unlock = false;
